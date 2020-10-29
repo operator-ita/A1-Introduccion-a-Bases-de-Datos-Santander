181fee83addc273e90fa32cfc7eed7b5e9533132
operator-ita <tesis.eliasd@gmail.com>
2020-10-28 20:48:01 -0600

    Retos_clase_01

---
+++ Sesion-02/Reto-01/reto1.sql
@@ -0,0 +1,13 @@
+use tienda; 
+SHOW TABLES;
+DESCRIBE products; 
+# ¿Qué artículos incluyen la palabra Pasta en su nombre?
+SELECT * FROM articulo WHERE nombre LIKE '%Pasta%'; 
+# ¿Qué artículos incluyen la palabra Cannelloni en su nombre?
+SELECT * FROM articulo WHERE nombre LIKE '%Cannelloni%'; 
+# ¿Qué nombres están separados por un guión (-) por ejemplo Puree - Kiwi?
+SELECT * FROM articulo WHERE nombre LIKE '%-%'; 
+# ¿Qué puestos incluyen la palabra Designer?
+SELECT * FROM puesto WHERE nombre LIKE '%Designer%'; 
+# ¿Qué puestos incluyen la palabra Developer?
+SELECT * FROM puesto WHERE nombre LIKE '%Developer%'; 
